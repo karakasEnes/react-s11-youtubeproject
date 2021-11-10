75fd849bdea55a6bf5fc23252f0432cc0dcab865
Enes Karakaş <enes.karakas.e@gamil.com>
2021-11-10 09:19:14 +0300

    video item with props pass, arrage img src etc

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,7 +1,16 @@
 import React from "react";
 
-const VideoItem = (props) => {
-  return <div>VideoItem</div>;
+const VideoItem = ({ video }) => {
+  console.log(video);
+  return (
+    <div>
+      <img
+        alt={video.snippet.title}
+        src={video.snippet.thumbnails.medium.url}
+      />
+      {video.snippet.title}
+    </div>
+  );
 };
 
 export default VideoItem;
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,7 +2,7 @@ import React from "react";
 import VideoItem from "./VideoItem";
 
 const VideoList = ({ videos }) => {
-  const renderedList = videos.map((video) => <VideoItem />);
+  const renderedList = videos.map((video) => <VideoItem video={video} />);
   return <div>{renderedList}</div>;
 };
 
