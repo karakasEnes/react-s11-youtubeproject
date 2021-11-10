bbc76ef0de15a793ebe29a8f9b7fffbe0d740ed0
Enes Karakaş <enes.karakas.e@gamil.com>
2021-11-10 09:53:55 +0300

    add style to video Items

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,14 +1,19 @@
 import React from "react";
+import "./VideoItem.css";
 
 const VideoItem = ({ video }) => {
   console.log(video);
   return (
-    <div>
+    <div className="video-item item">
       <img
+        className="ui image"
         alt={video.snippet.title}
         src={video.snippet.thumbnails.medium.url}
       />
-      {video.snippet.title}
+
+      <div className="content">
+        <div className="header">{video.snippet.title}</div>
+      </div>
     </div>
   );
 };
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,8 +2,10 @@ import React from "react";
 import VideoItem from "./VideoItem";
 
 const VideoList = ({ videos }) => {
-  const renderedList = videos.map((video) => <VideoItem video={video} />);
-  return <div>{renderedList}</div>;
+  const renderedList = videos.map((video) => (
+    <VideoItem key={video.id.videoId} video={video} />
+  ));
+  return <div className="ui list">{renderedList}</div>;
 };
 
 export default VideoList;
