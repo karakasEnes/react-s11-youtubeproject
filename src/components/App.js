dffe07b50a8e2b66a0b78f5d7ced2d53659bcc35
Enes Karakaş <enes.karakas.e@gamil.com>
2021-11-10 08:44:14 +0300

    axios setup, env setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,25 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import youtube from "../api/youtube";
+
 class App extends React.Component {
-  state = {};
-  handleSubmitForm = (search) => {
-    console.log(search);
+  state = { videos: [] };
+
+  handleSubmitForm = async (search) => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: search,
+      },
+    });
+
+    this.setState({ videos: response.data.items });
   };
 
   render() {
     return (
       <div>
         <SearchBar onSubmitForm={this.handleSubmitForm} />
+        <p>I have {this.state.videos.length} videos.</p>
       </div>
     );
   }
